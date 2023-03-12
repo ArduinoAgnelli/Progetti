9781ece6f86546e25f7a43bf70d39fbd08415bde
Marco-Cellini <99402338+MarcoCellini@users.noreply.github.com>
2023-03-12 12:12:11 +0100

    Update pirati-caraibi.cpp

--- pirati-caraibi/pirati-caraibi.cpp
+++ pirati-caraibi/pirati-caraibi.cpp
@@ -8,29 +8,34 @@
 #define DO5 523
 #define RE5 587
 
-#define CROMA 500
-#define SEMICROMA 250
+#define CROMA 400
+#define SEMICROMA 200
 
-int tema1 = [MI4, SOL4, LA4, LA4, LA4, SI4, DO5, DO5, DO5, RE5, SI4, SI4, LA4, SOL4, SOL4, LA4];
-int tema2 = [MI4, SOL4, LA4, LA4, LA4, SI4, DO5, DO5, DO5, RE5, SI4, SI4, LA4, SOL4, LA4];
-int pause1 = [SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, SEMICROMA, CROMA+SEMICROMA];
-int pause1 = [SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA];
+int tema1[] = {MI4, SOL4, LA4, LA4, LA4, SI4, DO5, DO5, DO5, RE5, SI4, SI4, LA4, SOL4, SOL4, LA4};
+int tema2[] = {MI4, SOL4, LA4, LA4, LA4, SI4, DO5, DO5, DO5, RE5, SI4, SI4, LA4, SOL4, LA4};
+int pause1[] = {SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, SEMICROMA, CROMA+SEMICROMA};
+int pause2[] = {SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA, CROMA, SEMICROMA, SEMICROMA, CROMA};
 
 #define BUZZER 2
 
 void setup() {
   pinMode(BUZZER, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  for (int i = 0; i < sizeof(tema1) / 4, i++) {
-    tone(BUZZER, tema1[i], pause1[i]);
-    NoTone(BUZZER);
+  Serial.print(sizeof(pause1));
+  
+  for (int i = 0; i < sizeof(tema1) / 2; i++) {
+    tone(BUZZER, tema1[i]);
+    delay(pause1[i]);
+    noTone(BUZZER);
   }
 
-  for (int i = 0; i < sizeof(tema2) / 4, i++) {
-    tone(BUZZER, tema2[i], pause2[i]);
-    NoTone(BUZZER);
+  for (int i = 0; i < sizeof(tema2) / 2; i++) {
+    tone(BUZZER, tema2[i]);
+    delay(pause1[i]);
+    noTone(BUZZER);
   }
 
   delay(CROMA);
